80ca9845730579a9380b379460c4bb114ad76d9c
Tarun <sahutarun50@gmail.com>
2026-07-12 01:16:43 +0530

    Initial Commit

---
+++ Penny/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Penny
+//
+//  Created by Tarun Sahu on 12/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Penny/PennyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PennyApp.swift
+//  Penny
+//
+//  Created by Tarun Sahu on 12/07/26.
+//
+
+import SwiftUI
+
+@main
+struct PennyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
