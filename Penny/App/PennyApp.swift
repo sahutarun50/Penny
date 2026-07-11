5f1aba473ee7e6e923c8d4a960f5b857e1280f86
Tarun <sahutarun50@gmail.com>
2026-07-12 02:37:51 +0530

    feat: complete project foundation setup

--- Penny/App/PennyApp.swift
+++ Penny/App/PennyApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct PennyApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LaunchView()
         }
     }
 }
---
+++ Penny/Core/Theme/AppTypography.swift
@@ -0,0 +1,8 @@
+//
+//  AppTypography.swift
+//  Penny
+//
+//  Created by Tarun Sahu on 12/07/26.
+//
+
+import Foundation
